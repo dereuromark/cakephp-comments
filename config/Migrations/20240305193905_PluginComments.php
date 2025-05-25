b82fe6455103141a2c612fca098b5a977ba9f0d4
mscherer <dereuromark@web.de>
2025-05-25 16:00:17 +0200

    Fix up BaseMigration file

--- config/Migrations/20240305193905_PluginComments.php
+++ config/Migrations/20240305193905_PluginComments.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
 
-use Migrations\AbstractMigration;
+use Migrations\BaseMigration;
 
-class PluginComments extends AbstractMigration {
+class PluginComments extends BaseMigration {
 
 	/**
 	 * Change Method.
